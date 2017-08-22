6ec8e9840c12475210ae8304f2c3e111c7707bbf
nexus <github.com@cyberstalker.eu>
2017-08-22 11:10:49 +0200

    Refactoring firmware install mechanism: - rename kernel package to linux-image-gpdpocket - let kernel provide and replace linux-firmware - merge gpdpocket-wireless into kernel package - provide control and script files as plaintext to be able to track it properly on git

--- packages/linux-image-gpdpocket/make.py
+++ packages/linux-image-gpdpocket/make.py
@@ -10,12 +10,12 @@ class Config(object):
     temp = os.path.abspath("tmp")
     kernel = os.path.abspath("files/bootstrap-kernel-boot.tar.bz2")
     modules = os.path.abspath("files/bootstrap-kernel-lib.tar.bz2")
-    templates = os.path.abspath("files/templates.tar.bz2")
+    templates = os.path.abspath("files/DEBIAN")
     versionPrefix = ".1"
     variables = {
         "architecture": "amd64",
         "maintainer": "Falk Garbsch <github.com@cyberstalker.eu>",
-        "name": "linux-image",
+        "name": "linux-image-gpdpocket",
     }
 
 print "first we cleanup our stuff"
@@ -33,10 +33,6 @@ os.makedirs(config.output)
 os.makedirs(config.build)
 os.makedirs(config.manifest)
 
-print "extract templates"
-templates = tarfile.open(config.templates, "r")
-templates.extractall(config.temp)
-
 print "extract kernel"
 kernel = tarfile.open(config.kernel, "r")
 kernel.extractall(config.build)
@@ -51,13 +47,23 @@ versions = [x[1] for x in filter(lambda x: x[0] == 'lib/modules', [(os.path.dirn
 print ">> found kernel versions: %s" % (", ".join(versions))
 assert(len(versions) == 1)
 
-print "fixing firmware directory"
-shutil.move(config.build + "/lib/firmware", config.build + "/lib/" + versions[0])
-os.makedirs(config.build + "/lib/firmware")
-shutil.move(config.build + "/lib/" + versions[0], config.build + "/lib/firmware")
+print "patching brcmfmac4356 firmware"
+copylist = [
+    ( 'files/brcmfmac4356-pcie.bin', '/lib/firmware/brcm/brcmfmac4356-pcie.bin', 0644 ),
+    ( 'files/brcmfmac4356-pcie.txt', '/lib/firmware/brcm/brcmfmac4356-pcie.txt', 0644 ),
+]
+for src, dst, mode in copylist:
+    print ">> copy (0%o) %s" % (mode, dst)
+    src = os.path.abspath(src)
+    dst = config.build + dst
+    dn = os.path.dirname(dst)
+    if not os.path.isdir(dn):
+        os.makedirs(dn)
+    shutil.copy(src, dst)
+    os.chmod(dst, mode)
 
 print "constructing control file"
-control = open(config.temp + "/control", "rb").read()
+control = open(config.templates + "/control", "rb").read()
 variables = config.variables
 variables["versionName"] = versions[0]
 variables["versionCode"] = "-".join(versions[0].split("-")[:-1]) + config.versionPrefix
@@ -69,9 +75,9 @@ fp.close()
 
 print "constructing script files"
 for script in ["/postinst", "/postrm", "/preinst", "/prerm"]:
-    print ">> write debian%s" % (script)
+    print ">> write DEBIAN%s" % (script)
     filepath = config.manifest + script
-    content = open(config.temp + script, "rb").read()
+    content = open(config.templates + script, "rb").read()
     fp = open(filepath, "wb")
     fp.write(content.replace("__VERSION_CODE__", variables["versionName"]))
     fp.flush()
