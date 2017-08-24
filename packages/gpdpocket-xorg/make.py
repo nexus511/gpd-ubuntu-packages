7dede0243edd9e98b4beac739ac825098d360098
nexus <github.com@cyberstalker.eu>
2017-08-24 13:38:59 +0200

    Ensure that xinput is present too.

--- packages/gpdpocket-xorg/make.py
+++ packages/gpdpocket-xorg/make.py
@@ -9,7 +9,7 @@ class Config(object):
     output = os.path.abspath("output")
     manifest = os.path.abspath("build/DEBIAN")
     temp = os.path.abspath("tmp")
-    version = "0.0.1"
+    version = "0.0.2"
     variables = {
         "architecture": "all",
         "maintainer": "Falk Garbsch <github.com@cyberstalker.eu>",
