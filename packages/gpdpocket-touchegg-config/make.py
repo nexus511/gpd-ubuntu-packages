a8dc9dae1e229a7d13de2ec9d923fdc94b60a525
nexus <github.com@cyberstalker.eu>
2017-08-25 16:30:22 +0200

    Add touchegg configuration package. This pulls touchegg and creates a default configuration.

--- packages/gpdpocket-touchegg-config/make.py
+++ packages/gpdpocket-touchegg-config/make.py
@@ -9,11 +9,12 @@ class Config(object):
     output = os.path.abspath("output")
     manifest = os.path.abspath("build/DEBIAN")
     temp = os.path.abspath("tmp")
-    version = "0.0.3"
+    templates = os.path.abspath("files/DEBIAN")
+    version = "0.0.1"
     variables = {
         "architecture": "all",
         "maintainer": "Falk Garbsch <github.com@cyberstalker.eu>",
-        "name": "gpdpocket",
+        "name": "gpdpocket-touchegg-config",
     }
 
 print "first we cleanup our stuff"
@@ -31,6 +32,22 @@ os.makedirs(config.output)
 os.makedirs(config.build)
 os.makedirs(config.manifest)
 
+print "copy files"
+copylist = [
+    ( 'files/touchegg.conf', '/usr/share/touchegg/touchegg.conf.gpd-pocket', 0644 ),
+    ( 'files/01_touchegg', '/etc/X11/Xsession.d/01_touchegg', 0755 ),
+    ( 'files/01_touchegg', '/etc/X11/xinit/xinitrc.d/01_touchegg', 0755 ),
+]
+for src, dst, mode in copylist:
+    print ">> copy (0%o) %s" % (mode, dst)
+    src = os.path.abspath(src)
+    dst = config.build + dst
+    dn = os.path.dirname(dst)
+    if not os.path.isdir(dn):
+        os.makedirs(dn)
+    shutil.copy(src, dst)
+    os.chmod(dst, mode)
+
 print "write control"
 variables = config.variables
 variables["version"] = config.version
@@ -39,6 +56,17 @@ fp = open(config.manifest + "/control", "wb")
 fp.write(control.format(**variables))
 fp.flush()
 fp.close()
+
+print "constructing script files"
+for script in [ "/preinst", "/postrm" ]:
+    print ">> write DEBIAN%s" % (script)
+    filepath = config.manifest + script
+    content = open(config.templates + script, "rb").read()
+    fp = open(filepath, "wb")
+    fp.write(content.replace("__VERSION_CODE__", variables["version"]))
+    fp.flush()
+    fp.close()
+    os.chmod(filepath, 0555)
 
 print "building binary package"
 command = ["fakeroot", "dpkg-deb", "-b", config.build]
